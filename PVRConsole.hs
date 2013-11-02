a3a9c6ae45474d9ce95ad564eb919130f7dfcd52
Edmondo Pentangelo <epentangelo@AspireOne.(none)>
2013-11-02 00:04:35 +0000

    Added config file to data-files in PVRConsole.cabal and updated code to load fileName from cabal generated Paths_app. Added a dummy Paths_app.hs for interpretation in GHCi

--- PVRConsole.hs
+++ PVRConsole.hs
@@ -14,30 +14,30 @@ import System.Locale
 import System.Environment
 import Data.Configurator
 import Data.Configurator.Types
+import Paths_app
 
+data Entries = Entries { totalCount :: Int, 
+                         entries :: [Entry] } 
+                         deriving (Show, Generic)
 
-data Upcoming = Upcoming { totalCount :: Int, 
-                           entries :: [UpcomingEntry] } 
-                           deriving (Show, Generic)
+data Entry = Entry { channel :: !Text, 
+                     title :: !Text, 
+                     start :: Int } 
+                     deriving (Show, Generic)
 
-data UpcomingEntry = UpcomingEntry { channel :: !Text, 
-                                     title :: !Text, 
-                                     start :: Int } 
-                                     deriving (Show, Generic)
-
-instance FromJSON Upcoming 
+instance FromJSON Entries 
         
-instance FromJSON UpcomingEntry 
+instance FromJSON Entry 
 
 jsonURL :: String -> String -> String
-jsonURL host cmd = printf "http://%s:9981/dvrlist_%s" host cmd
+jsonURL host cmd = printf "http://%s:9981/%s" host cmd
 
 getJSON :: String -> String -> IO BS.ByteString
 getJSON host cmd = simpleHttp (jsonURL host cmd)
 
-getPvrData :: String -> String -> IO (Maybe Upcoming)
+getPvrData :: String -> String -> IO (Maybe Entries)
 getPvrData host cmd = do v <- getJSON host cmd
-                         return (decode v :: Maybe Upcoming)
+                         return (decode v :: Maybe Entries)
 
 getTime :: Int -> UTCTime
 getTime secs = posixSecondsToUTCTime $ fromIntegral secs
@@ -45,13 +45,13 @@ getTime secs = posixSecondsToUTCTime $ fromIntegral secs
 timeToString :: UTCTime -> String
 timeToString time = formatTime defaultTimeLocale "%c" time 
 
-getTitle :: UpcomingEntry -> String
+getTitle :: Entry -> String
 getTitle entry = printf "%s\n%s\n%s\n" ch tt st
                where ch = unpack (channel entry)
                      tt = unpack (title entry)
                      st = timeToString (getTime (start entry))
                        
-titles :: Upcoming -> String
+titles :: Entries -> String
 titles xs = foldl f "" (entries xs)
           where f acc x = printf "%s\n%s" acc (getTitle x)
 
@@ -61,7 +61,8 @@ getString _ = error "Expected a string"
 
 main :: IO ()
 main =  do xs <- getArgs
-           cfg <- load [Required "PVRConsole.cfg"]
+           file <- getDataFileName "PVRConsole.cfg"
+           cfg <- load [Required file]
            host <-require cfg "host" :: IO Value                                                             
            v <- getPvrData (getString host) (head xs)
            putStrLn (titles (fromJust v))
